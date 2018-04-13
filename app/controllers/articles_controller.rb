3b440c04fb8f7ab142c43a02f7a79b3474bff6ef
Luke Stoss <luke.stoss@gmail.com>
2018-04-13 17:56:58 -0500

    Fixed mistake with controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :require_login, except: [:index, :show]
+
   include ArticlesHelper
 
   def index
@@ -7,8 +9,9 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+
     @comment = Comment.new
-    @comment.article_id = @article_id
+    @comment.article_id = @article.id
   end
 
   def new
@@ -18,14 +21,18 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
     flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
-    flash.notice = "Article '#{@article.title}' Deleted!"
+
+    flash.notice = "Article '#{@article.title}' Destroyed!"
+
     redirect_to articles_path
   end
 
@@ -36,7 +43,10 @@ class ArticlesController < ApplicationController
   def update
     @article = Article.find(params[:id])
     @article.update(article_params)
+
     flash.notice = "Article '#{@article.title}' Updated!"
+
     redirect_to article_path(@article)
   end
+
 end
